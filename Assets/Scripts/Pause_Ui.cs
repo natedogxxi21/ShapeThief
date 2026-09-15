5b00854965525f0db30c09b3b81ff0a09db0db09
Naughtdread <Naughtdread682@yahoo.com>
2026-09-14 19:13:26 -0700

    Pause ui
    Pause screen un pauses when resume is pressed

--- Assets/Scripts/Pause_Ui.cs
+++ Assets/Scripts/Pause_Ui.cs
@@ -8,15 +8,30 @@ public class Pause_Ui : MonoBehaviour
         Debug.Log("GAME PAUSED - Time Scale: " + Time.timeScale);
     }
 
-   
+
     public void CloseUpgradeScreen()
-    { 
+    {
         gameObject.SetActive(false);
         Time.timeScale = 1f;
+        Debug.Log("GAME RESUMED - Time Scale: " + Time.timeScale);
     }
     public void Upgrade1()
     {
         //Upgrade Code will go here but for now it just closes ui!
+        CloseUpgradeScreen();
+    }
+    public void Upgrade2()
+    {
+        //Upgrade Code will go here but for now it just closes ui!
+        CloseUpgradeScreen();
+    }
+    public void Upgrade3()
+    {
+        //Upgrade Code will go here but for now it just closes ui!
+        CloseUpgradeScreen();
+    }
+    public void PauseGame()
+    {
         CloseUpgradeScreen();
     }
 }
